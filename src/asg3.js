ce39bceb132b0b3ed6053d6183562a07234fb911
Justin Lam <justinflam9225k@gmail.com>
2025-02-14 15:24:30 -0800

    Added a map, cleaned up code

--- src/asg3.js
+++ src/asg3.js
@@ -4,12 +4,11 @@ const VSHADER_SOURCE = `
 	attribute vec2 a_UV;
 	varying vec2 v_UV;
 	uniform mat4 u_ModelMatrix;
-	uniform mat4 u_GlobalRotationMatrix;
 	uniform mat4 u_ViewMatrix;
 	uniform mat4 u_ProjectionMatrix;
 
 	void main() {
-		gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_GlobalRotationMatrix * u_ModelMatrix * a_Position;
+		gl_Position = u_ProjectionMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
 		v_UV = a_UV;
 	}
 `;
@@ -33,16 +32,61 @@ let canvas;
 let gl;
 let camera;
 
+const map = [	// 32x32x4
+	//                                            m  m
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],// m
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],// m
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
 let a_Position;
 let a_UV;
 let u_ModelMatrix;
-let u_GlobalRotationMatrix;
 let u_ViewMatrix;
 let u_ProjectionMatrix;
 
 let u_FragColor;
 let u_Sampler0;
 let u_TextureType;
+
+function main() {
+	getGlobalVars();
+	setupWebGL();
+	initTextures();
+	document.onkeydown = (e) => onKeydown(e);
+	gl.clearColor(0,0,0,1);	// black
+
+	requestAnimationFrame(tick);
+}
 
 /** Gets this.canvas, this.gl, and this.camera. */
 function getGlobalVars() {
@@ -53,6 +97,9 @@ function getGlobalVars() {
 	gl.enable(gl.DEPTH_TEST);
 
 	camera = new Camera();
+	const translation = new Vector3([0,1,0]);
+	camera.eye.add(translation);
+	camera.at.add(translation);
 }
 
 /** Compiles shaders and links GLSL ES variables. */
@@ -70,10 +117,6 @@ function setupWebGL() {
 	u_ModelMatrix = gl.getUniformLocation(gl.program, "u_ModelMatrix");
 	if (!u_ModelMatrix) throw new Error("Failed to get the storage location of u_ModelMatrix.");
 	gl.uniformMatrix4fv(u_ModelMatrix, false, identityMatrix.elements);
-
-	u_GlobalRotationMatrix = gl.getUniformLocation(gl.program, "u_GlobalRotationMatrix");
-	if (!u_GlobalRotationMatrix) throw new Error("Failed to get the storage location of u_GlobalRotationMatrix.");
-	gl.uniformMatrix4fv(u_GlobalRotationMatrix, false, identityMatrix.elements);
 
 	u_ViewMatrix = gl.getUniformLocation(gl.program, "u_ViewMatrix");
 	if (!u_ViewMatrix) throw new Error("Failed to get the storage location of u_ViewMatrix.");
@@ -121,77 +164,6 @@ function sendTextureTo_TEXTURE0(image) {
 	gl.uniform1i(u_Sampler0, 0);
 }
 
-let g_globalRotation_y = 0;	// y axis
-let g_globalRotation_x = 0;	// x axis
-let g_headRotation = 0;
-let g_headScale = 1;
-let g_animation_enabled_head = false;
-let g_tongueBaseRotation = 0;
-let g_animation_enabled_tongueBase = false;
-let g_tongueTipRotation = 0;
-let g_animation_enabled_tongueTip = false;
-let g_leg_front_leftRotation = 0;
-let g_leg_front_rightRotation = 0;
-let g_leg_back_leftRotation = 0;
-let g_leg_back_rightRotation = 0;
-let g_animation_enabled_legs = false;
-let g_interactiveAnimationPlaying = false;
-
-function createUIEvents() {
-	document.getElementById("globalRotationSlider_y").addEventListener("mousemove", function() {
-		g_globalRotation_y = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("globalRotationSlider_x").addEventListener("mousemove", function() {
-		g_globalRotation_x = this.value;
-		renderAllShapes();
-	});
-
-	document.getElementById("headRotationSlider").addEventListener("mousemove", function() {
-		g_headRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("toggleAnimationButton_Head").onclick = () => g_animation_enabled_head = !g_animation_enabled_head;
-	document.getElementById("tongueBaseRotationSlider").addEventListener("mousemove", function() {
-		g_tongueBaseRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("toggleAnimationButton_TongueBase").onclick = () => g_animation_enabled_tongueBase = !g_animation_enabled_tongueBase;
-	document.getElementById("tongueTipRotationSlider").addEventListener("mousemove", function() {
-		g_tongueTipRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("toggleAnimationButton_TongueTip").onclick = () => g_animation_enabled_tongueTip = !g_animation_enabled_tongueTip;
-
-	document.getElementById("leg_front_leftRotationSlider").addEventListener("mousemove", function() {
-		g_leg_front_leftRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("leg_front_rightRotationSlider").addEventListener("mousemove", function() {
-		g_leg_front_rightRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("leg_back_leftRotationSlider").addEventListener("mousemove", function() {
-		g_leg_back_leftRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("leg_back_rightRotationSlider").addEventListener("mousemove", function() {
-		g_leg_back_rightRotation = this.value;
-		renderAllShapes();
-	});
-	document.getElementById("toggleAnimationButton_Legs").onclick = () => g_animation_enabled_legs = !g_animation_enabled_legs;
-}
-
-function main() {
-	getGlobalVars();
-	setupWebGL();
-	initTextures();
-	createUIEvents();
-	document.onkeydown = (e) => onKeydown(e);
-	gl.clearColor(0,0,0,1);	// black
-	requestAnimationFrame(tick);
-}
-
 function onKeydown(e) {
 	if (e.key === 'w') camera.moveForward();
 	else if (e.key === 's') camera.moveBackward();
@@ -199,66 +171,16 @@ function onKeydown(e) {
 	else if (e.key === 'd') camera.moveRight();
 	else if (e.key === 'q') camera.panLeft();
 	else if (e.key === 'e') camera.panRight();
-	renderAllShapes();
+	render();
 }
 
-const g_startTime = currentTime();
-let g_elapsedTime = 0;
-const g_interactiveAnimationDuration = 1;
-let g_interactiveAnimationStartTime = 0;
-function currentTime() {
-	return performance.now() / 1000.0;
-}
 function tick() {
-	g_elapsedTime = currentTime() - g_startTime;
-
-	if (g_interactiveAnimationPlaying) {
-		updateInteractiveAnimation();
-	}
-	else {
-		updateAnimationAngles();
-	}
-
-	renderAllShapes();
+	render();
 	requestAnimationFrame(tick);
 }
 
-function updateInteractiveAnimation() {
-	g_headScale += 0.01;
-	const counter = g_elapsedTime - g_interactiveAnimationStartTime;
-	if (counter >= g_interactiveAnimationDuration) {
-		g_headScale = 1;
-		g_interactiveAnimationPlaying = false;
-	}
-}
-
-function updateAnimationAngles() {
-	if (g_animation_enabled_head) {
-		g_headRotation = 45 * Math.sin(g_elapsedTime);
-	}
-	if (g_animation_enabled_tongueBase) {
-		g_tongueBaseRotation = 15 * Math.sin(g_elapsedTime * 20);
-	}
-	if (g_animation_enabled_tongueTip) {
-		g_tongueTipRotation = 30 * Math.sin(g_elapsedTime * 20);
-	}
-	if (g_animation_enabled_legs) {
-		g_leg_front_leftRotation = 45 * Math.sin(g_elapsedTime * 4);
-		g_leg_back_leftRotation = 45 * Math.sin(g_elapsedTime * 4);
-		g_leg_front_rightRotation = 45 * Math.sin(g_elapsedTime * 4 + Math.PI);
-		g_leg_back_rightRotation = 45 * Math.sin(g_elapsedTime * 4 + Math.PI);
-	}
-}
-
-let g_eye = [0, 0, 3];
-let g_at = [0, 0, -100];
-let g_up = [0, 1, 0];
-
-function renderAllShapes() {
-	const globalRotationMatrix = new Matrix4();
-	globalRotationMatrix.rotate(g_globalRotation_x, 1, 0, 0);
-	globalRotationMatrix.rotate(-g_globalRotation_y, 0, 1, 0);
-	gl.uniformMatrix4fv(u_GlobalRotationMatrix, false, globalRotationMatrix.elements);
+/** Renders the sky, floor, and map. */
+function render() {
 	gl.uniformMatrix4fv(u_ViewMatrix, false, camera.viewMatrix.elements);
 	gl.uniformMatrix4fv(u_ProjectionMatrix, false, camera.projectionMatrix.elements);
 
@@ -272,90 +194,19 @@ function renderAllShapes() {
 	sky.render();
 
 	const floor = new Cube();
-	floor.matrix.translate(0, -0.5, 0);
 	floor.matrix.scale(32, 0, 32);	// scale y to 0 makes a plane
 	floor.matrix.translate(-0.5, -0.5, -0.5);
 	floor.render();
 
-	const body = new Cube();
-	body.color = [1, 0, 0.8, 1];	// pink
-	body.matrix.translate(-0.25, -0.25, 0);
-	body.matrix.scale(0.5, 0.5, 1);
-	body.render();
-
-	const leg_front_left = new Cube();
-	leg_front_left.color = [1, 0.2, 0.8, 1];	// other pink
-	leg_front_left.matrix.translate(0.15, -0.2, 0.2);
-	leg_front_left.matrix.rotate(-g_leg_front_leftRotation, 1, 0, 0);
-	leg_front_left.matrix.scale(0.2, 0.3, 0.2);
-	leg_front_left.matrix.translate(-0.5, -1, -0.5);
-	leg_front_left.render();
-
-	const leg_front_right = new Cube();
-	leg_front_right.color = [1, 0.2, 0.8, 1];	// other pink
-	leg_front_right.matrix.translate(-0.15, -0.2, 0.2);
-	leg_front_right.matrix.rotate(-g_leg_front_rightRotation, 1, 0, 0);
-	leg_front_right.matrix.scale(0.2, 0.3, 0.2);
-	leg_front_right.matrix.translate(-0.5, -1, -0.5);
-	leg_front_right.render();
-
-	const leg_back_left = new Cube();
-	leg_back_left.color = [1, 0.2, 0.8, 1];	// other pink
-	leg_back_left.matrix.translate(0.15, -0.2, 0.95);
-	leg_back_left.matrix.rotate(-g_leg_back_leftRotation, 1, 0, 0);
-	leg_back_left.matrix.scale(0.2, 0.3, 0.2);
-	leg_back_left.matrix.translate(-0.5, -1, -0.5);
-	leg_back_left.render();
-
-	const leg_back_right = new Cube();
-	leg_back_right.color = [1, 0.2, 0.8, 1];	// other pink
-	leg_back_right.matrix.translate(-0.15, -0.2, 0.95);
-	leg_back_right.matrix.rotate(-g_leg_back_rightRotation, 1, 0, 0);
-	leg_back_right.matrix.scale(0.2, 0.3, 0.2);
-	leg_back_right.matrix.translate(-0.5, -1, -0.5);
-	leg_back_right.render();
-
-	const head = new Cube();
-	head.color = [1, 0.2, 0.8, 1];	// other pink
-	head.matrix.translate(0, 0.15, -0.3);
-	head.matrix.rotate(g_headRotation, 0, 0, 1);
-	head.matrix.scale(g_headScale, g_headScale, g_headScale);
-	const headCoordsMatrix = new Matrix4(head.matrix);
-	head.matrix.scale(0.4, 0.4, 0.4);
-	head.matrix.translate(-0.5, -0.5, 0);
-	head.render();
-
-	const eye_left = new Cube();
-	eye_left.color = [0, 0, 0, 1];	// black
-	eye_left.matrix = new Matrix4(headCoordsMatrix);
-	eye_left.matrix.translate(0.05, 0, -0.025);
-	eye_left.matrix.scale(0.1, 0.1, 0.1);
-	eye_left.render();
-
-	const eye_right = new Cube();
-	eye_right.color = [0, 0, 0, 1];	// black
-	eye_right.matrix = new Matrix4(headCoordsMatrix);
-	eye_right.matrix.translate(-0.15, 0, -0.025);
-	eye_right.matrix.scale(0.1, 0.1, 0.1);
-	eye_right.render();
-
-	const tongue_base = new Cube();
-	tongue_base.color = [1, 0, 0, 1];	// red
-	tongue_base.matrix = new Matrix4(headCoordsMatrix);
-	tongue_base.matrix.translate(0, -0.15, 0.025);
-	tongue_base.matrix.rotate(g_tongueBaseRotation, 1, 0, 0);
-	tongue_base.matrix.rotate(60, 1, 0, 0);
-	const tongueBaseCoordsMatrix = new Matrix4(tongue_base.matrix);
-	tongue_base.matrix.scale(0.2, 0.1, 0.05);
-	tongue_base.matrix.translate(-0.5, -1, -0.5);
-	tongue_base.render();
-
-	const tongue_tip = new Cube();
-	tongue_tip.color = [1, 0, 0, 1];	// red
-	tongue_tip.matrix = new Matrix4(tongueBaseCoordsMatrix);
-	tongue_tip.matrix.translate(0, -0.1, 0);
-	tongue_tip.matrix.rotate(g_tongueTipRotation, 1, 0, 0);
-	tongue_tip.matrix.scale(0.2, 0.1, 0.05);
-	tongue_tip.matrix.translate(-0.5, -1, -0.5);
-	tongue_tip.render();
+	for (let y = 0; y < map.length; y++) {
+		for (let x = 0; x < map[0].length; x++) {
+			const wallHeight = map[y][x];
+			for (h = 0; h < wallHeight; h++) {
+				const cube = new Cube();
+				cube.textureType = 0;
+				cube.matrix.translate(x-16, h, y-16);
+				cube.render();
+			}
+		}
+	}
 }
